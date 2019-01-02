24ff8028dc71527ef657bcf09711993cac332a4e
windcoder <zhrucong@gmail.com>
2019-01-03 01:41:40 +0800

    调整登录

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
-var host = 'https://book.windcoder.com/nightbook';
-
+// var host = 'https://book.windcoder.com/nightbook';
+// var host = 'http://localhost:8080';
+var host = 'http://localhost:8000';
 var config = {
   pid:1,
   // 下面的地址配合云端 Demo 工作
@@ -9,7 +10,7 @@ var config = {
     // 登录地址，用于建立会话
     // loginUrl: `https://${host}/login`,
     loginUrl: `${host}/auth/home`,
-    apiUrlhead: `${host}/jsapi/`,
+    apiUrlhead: `${host}/jsapi/mina/`,
     // 测试的请求地址，用于测试会话
     requestUrl: `https://${host}/user`,
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -629,4 +629,12 @@ Page({
       }
     }
   },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
 });
